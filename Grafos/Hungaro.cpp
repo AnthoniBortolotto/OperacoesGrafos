f3c8a05c07579a777a6474bda03f6792162fd2f9
Felipe <felipe.mattes@gamatec.com.br>
2021-10-12 17:05:26 -0300

    refatoração

--- Grafos/Hungaro.cpp
+++ Grafos/Hungaro.cpp
@@ -3,15 +3,15 @@
 
 #include <utility>
 
-Hungaro::Hungaro(vector<vector<int>> matriz) {
+Hungaro::Hungaro(const vector<vector<int>>& matriz) {
     this->valoresOriginais = igualarLinhaColuna(matriz);
-    valores = igualarLinhaColuna(clonarMatriz(matriz));
+    this->valores = igualarLinhaColuna(clonarMatriz(matriz));
 
-    linhasMatriz.resize(valores[0].size(), 0);
-    colunasOcupadas.resize(valores[0].size(), 0);
+    this->linhasMatriz.resize(this->valores[0].size(), 0);
+    this->colunasOcupadas.resize(this->valores[0].size(), 0);
 
     cout << "Matriz. " << endl;
-    printarMatriz(valores);
+    printarMatriz(this->valores);
 
     //Passo 1
     subtrairLinhaValorMinimo();
@@ -20,7 +20,7 @@ Hungaro::Hungaro(vector<vector<int>> matriz) {
     //Passo 3
     cobrirZeros();
     //Passo 4
-    while(numeroLinhas < valores.size())
+    while(this->numeroLinhas < this->valores.size())
     {
         criarZerosAdicionais();
         cobrirZeros();
@@ -33,7 +33,7 @@ Hungaro::Hungaro(vector<vector<int>> matriz) {
     cout << "printando Matriz Valores::: " << endl;
     printarMatriz(valores);
 
-    cout << "Valor Total : " << getTotal() << endl;
+    cout << endl << "Valor Total : " << getTotal() << endl;
 }
 
 vector<vector<int>> Hungaro::clonarMatriz(vector<vector<int>> matriz) {
@@ -42,74 +42,74 @@ vector<vector<int>> Hungaro::clonarMatriz(vector<vector<int>> matriz) {
 }
 
 void Hungaro::subtrairLinhaValorMinimo() {
-    vector<int> menorValorLinha(valores.size(), -1);
+    vector<int> menorValorLinha(this->valores.size(), -1);
 
     //Percorrendo a matriz para verificar o menor valor linha.
-    for(int linha = 0; linha < valores.size(); linha++)
+    for(int linha = 0; linha < this->valores.size(); linha++)
     {
-        menorValorLinha[linha] = valores[linha][0]; //instanciando o primeiro valor como menor
-        for(int col = 1; col < valores[0].size(); col++)
+        menorValorLinha[linha] = this->valores[linha][0]; //instanciando o primeiro valor como menor
+        for(int col = 1; col < this->valores[0].size(); col++)
         {
-            if(valores[linha][col] < menorValorLinha[linha])
-                menorValorLinha[linha] = valores[linha][col];
+            if(this->valores[linha][col] < menorValorLinha[linha])
+                menorValorLinha[linha] = this->valores[linha][col];
         }
     }
 
     //Percorrendo matriz para realizar subtração do menor valor linha.
-    for(int linha = 0; linha < valores.size(); linha++)
+    for(int linha = 0; linha < this->valores.size(); linha++)
     {
-        for(int col = 0; col < valores[0].size(); col++)
+        for(int col = 0; col < this->valores[0].size(); col++)
         {
-            valores[linha][col] -= menorValorLinha[linha];
+            this->valores[linha][col] -= menorValorLinha[linha];
         }
     }
 
     cout << "Subtraindo por linha. " << endl;
-    printarMatriz(valores);
+    printarMatriz(this->valores);
 }
 
 void Hungaro::subtrairColunaValorMinimo() {
-    vector<int> menorValorColuna(valores[0].size(), -1);
+    vector<int> menorValorColuna(this->valores[0].size(), -1);
 
     //Percorrendo a matriz para verificar o menor valor coluna.
-    for(int col = 0; col < valores[0].size(); col++)
+    for(int col = 0; col < this->valores[0].size(); col++)
     {
-        menorValorColuna[col] = valores[0][col]; //primeiro valor como menor
-        for(int linha = 1; linha < valores.size(); linha++)
+        menorValorColuna[col] = this->valores[0][col]; //primeiro valor como menor
+        for(int linha = 1; linha < this->valores.size(); linha++)
         {
-            if(valores[linha][col] < menorValorColuna[col])
-                menorValorColuna[col] = valores[linha][col];
+            if(this->valores[linha][col] < menorValorColuna[col])
+                menorValorColuna[col] = this->valores[linha][col];
         }
     }
 
     //Percorrendo matriz para realizar subtração do menor valor coluna.
-    for(int col = 0; col < valores.size(); col++)
+    for(int col = 0; col < this->valores.size(); col++)
     {
-        for(int linha = 1; linha < valores.size(); linha++)
+        for(int linha = 1; linha < this->valores.size(); linha++)
         {
-            valores[linha][col] -= menorValorColuna[col];
+            this->valores[linha][col] -= menorValorColuna[col];
         }
     }
 
     cout << "Subtraindo por coluna. " << endl;
-    printarMatriz(valores);
+    printarMatriz(this->valores);
 }
 
 void Hungaro::cobrirZeros() {
-    numeroLinhas = 0;
+    this->numeroLinhas = 0;
 
     //Inicializando
-    linhas = valores;
+    this->linhas = this->valores;
 
-    for(int linha=0; linha<valores.size();linha++){
-        for(int col=0; col<valores.size();col++){
-            linhas[linha][col] = 0;
+    for(int linha=0; linha < this->valores.size();linha++){
+        for(int col=0; col < this->valores.size();col++){
+            this->linhas[linha][col] = 0;
         }
     }
 
-    for(int linha=0; linha<valores.size();linha++){
-        for(int col=0; col<valores.size();col++){
-            if(valores[linha][col] == 0)
+    for(int linha=0; linha < this->valores.size();linha++){
+        for(int col=0; col < this->valores.size();col++){
+            if(this->valores[linha][col] == 0)
                 selecionarVizinhos(linha, col, maxVerticalHorizontal(linha, col));
         }
     }
@@ -121,10 +121,10 @@ o valor resultante será negativo(horizontal), zero(horizontal) ou positivo(vert
 
 int Hungaro::maxVerticalHorizontal(int linha, int coluna) {
     int retorno = 0;
-    for(int i=0; i<valores.size();i++){
-        if(valores[i][coluna] == 0)
+    for(int i=0; i < this->valores.size();i++){
+        if(this->valores[i][coluna] == 0)
             retorno++;
-        if(valores[linha][i] == 0)
+        if(this->valores[linha][i] == 0)
             retorno--;
     }
     return retorno;
@@ -134,23 +134,23 @@ int Hungaro::maxVerticalHorizontal(int linha, int coluna) {
 void Hungaro::selecionarVizinhos(int linha, int coluna, int maximoVerticalHorizontal) {
 
     //Horizontal -1, vertical 1, intersecção 2;
-    if(linhas[linha][coluna] == 2) // se ja foi riscada duas vezes (intersecção) não risca novamente.
+    if(this->linhas[linha][coluna] == 2) // se ja foi riscada duas vezes (intersecção) não risca novamente.
         return;
 
-    if(maximoVerticalHorizontal > 0 && linhas[linha][coluna] == 1) // se foi riscada verticalmente, não risca novamente
+    if(maximoVerticalHorizontal > 0 && this->linhas[linha][coluna] == 1) // se foi riscada verticalmente, não risca novamente
         return;
 
-    if(maximoVerticalHorizontal <= 0 && linhas[linha][coluna] == -1) // se foi riscado horizontalmente não risca novamente
+    if(maximoVerticalHorizontal <= 0 && this->linhas[linha][coluna] == -1) // se foi riscado horizontalmente não risca novamente
         return;
 
-    for(int i=0; i<valores.size();i++){ // loop nos valores dos indices [linha][coluna] e os vizinhos respectivos
+    for(int i=0; i < this->valores.size();i++){ // loop nos valores dos indices [linha][coluna] e os vizinhos respectivos
         if(maximoVerticalHorizontal > 0)	// se o valor maximoVerticalHorizontal for positivo, risca verticalmente
-            linhas[i][coluna] = linhas[i][coluna] == -1 || linhas[i][coluna] == 2 ? 2 : 1; // se foi riscada antes da horizontal (-1), e precisa ser riscada na vertical (1), é uma intersecção (2). Caso contrario risca vertical.
+            this->linhas[i][coluna] = this->linhas[i][coluna] == -1 || this->linhas[i][coluna] == 2 ? 2 : 1; // se foi riscada antes da horizontal (-1), e precisa ser riscada na vertical (1), é uma intersecção (2). Caso contrario risca vertical.
         else			// se o valor maximoVerticalHorizontal é zero, risca horizontal.
-            linhas[linha][i] = linhas[linha][i] == 1 || linhas[linha][i] == 2 ? 2 : -1; // se foi riscada antes da vertical (1) e precisa ser riscada na horizontal (-1), é uma intersecção (2). Caso contrario risca horizontal.
+            this->linhas[linha][i] = this->linhas[linha][i] == 1 || this->linhas[linha][i] == 2 ? 2 : -1; // se foi riscada antes da vertical (1) e precisa ser riscada na horizontal (-1), é uma intersecção (2). Caso contrario risca horizontal.
     }
     // incrementando o numero de linha
-    numeroLinhas++;
+    this->numeroLinhas++;
 
 }
 
@@ -158,21 +158,21 @@ void Hungaro::criarZerosAdicionais() {
     int menorValorNaoRiscado = 0;
 
     //procura o valor minimo nos numeros não riscados
-    for(int linha=0; linha<valores.size();linha++){
-        for(int col=0; col<valores.size();col++){
-            if(linhas[linha][col] == 0 && (valores[linha][col] < menorValorNaoRiscado || menorValorNaoRiscado == 0))
-                menorValorNaoRiscado = valores[linha][col];
+    for(int linha=0; linha < this->valores.size();linha++){
+        for(int col=0; col < this->valores.size();col++){
+            if(this->linhas[linha][col] == 0 && (this->valores[linha][col] < menorValorNaoRiscado || menorValorNaoRiscado == 0))
+                menorValorNaoRiscado = this->valores[linha][col];
         }
     }
 
     // Subtrai o minimo dos valores não riscados, e então adiciona os elementos riscados duas vezes
-    for(int linha=0; linha<valores.size();linha++){
-        for(int col=0; col<valores[0].size();col++){
-            if(linhas[linha][col] == 0) // Se não estiver riscado, subtrai
-                valores[linha][col] -= menorValorNaoRiscado;
+    for(int linha=0; linha < this->valores.size();linha++){
+        for(int col=0; col < this->valores.size();col++){
+            if(this->linhas[linha][col] == 0) // Se não estiver riscado, subtrai
+                this->valores[linha][col] -= menorValorNaoRiscado;
 
-            else if(linhas[linha][col] == 2) // Se estiver riscado 2 vezes (intersecção), adiciona
-                valores[linha][col] += menorValorNaoRiscado;
+            else if(this->linhas[linha][col] == 2) // Se estiver riscado 2 vezes (intersecção), adiciona
+                this->valores[linha][col] += menorValorNaoRiscado;
         }
     }
 
@@ -199,6 +199,7 @@ vector<vector<int>> Hungaro::igualarLinhaColuna(vector<vector<int>> matriz) {
     else
     {
         vector<int> coluna;
+        coluna.reserve(matriz.size());
         for (int i = 0; i < matriz.size(); i++) {
             coluna.push_back(0);
         }
@@ -208,16 +209,16 @@ vector<vector<int>> Hungaro::igualarLinhaColuna(vector<vector<int>> matriz) {
 }
 
 bool Hungaro::optimizar(int linha) {
-    if(linha == linhasMatriz.size()) // se todas linhas ja forem designadas
+    if(linha == this->linhasMatriz.size()) // se todas linhas ja forem designadas
         return true;
 
     for(int col=0; col<valores.size();col++){ // Tenta todas colunas
-        if(valores[linha][col] == 0 && colunasOcupadas[col] == 0){ // Se o valor atual na coluna 'col' é zero e não está ocupada
-            linhasMatriz[linha] = col; // Atribui a linha com o valor da coluna
-            colunasOcupadas[col] = 1; // Marca a coluna como ocupada
+        if(this->valores[linha][col] == 0 && this->colunasOcupadas[col] == 0){ // Se o valor atual na coluna 'col' é zero e não está ocupada
+            this->linhasMatriz[linha] = col; // Atribui a linha com o valor da coluna
+            this->colunasOcupadas[col] = 1; // Marca a coluna como ocupada
             if(optimizar(linha+1)) // Se as proximas linhas foram atribuidas com sucesso em uma unica coluna.
                 return true;
-            colunasOcupadas[col] = 0; // Se as proximas linhas não possuem valor, retorna e tenta as outras.
+            this->colunasOcupadas[col] = 0; // Se as proximas linhas não possuem valor, retorna e tenta as outras.
         }
     }
     return false;
@@ -229,10 +230,12 @@ bool Hungaro::optimizar() {
 
 int Hungaro::getTotal() {
     int total = 0;
-    for(int i = 0; i < valores.size(); i++)
+    for(int i = 0; i < this->valores.size(); i++)
     {
-        total+= valoresOriginais[i][linhasMatriz[i]];
+        total+= this->valoresOriginais[i][this->linhasMatriz[i]];
+        cout << endl << this->valoresOriginais[i][this->linhasMatriz[i]];
     }
+
 
     return total;
 }
--- Grafos/Hungaro.h
+++ Grafos/Hungaro.h
@@ -6,13 +6,13 @@ using namespace std;
 
 class Hungaro {
 public:
-    Hungaro(vector<vector<int>> matriz);
+    explicit Hungaro(const vector<vector<int>>& matriz);
 
 private:
     vector<vector<int>> valoresOriginais;
     vector<vector<int>> valores;
     vector<vector<int>> linhas;
-    int numeroLinhas;
+    int numeroLinhas{};
     vector<int> linhasMatriz;
     vector<int> colunasOcupadas;
     static vector<vector<int>> clonarMatriz(vector<vector<int>> matriz);
@@ -27,10 +27,10 @@ private:
     void selecionarVizinhos(int linha, int coluna, int maximoVerticalHorizontal);
     void criarZerosAdicionais();
 
-    void printarMatriz(vector<vector<int>> matriz);
+    static void printarMatriz(vector<vector<int>> matriz);
     bool optimizar(int linha);
     bool optimizar();
-    vector<vector<int>> igualarLinhaColuna(vector<vector<int>> matriz);
+    static vector<vector<int>> igualarLinhaColuna(vector<vector<int>> matriz);
 
     int getTotal();
 };
